7f6aeb032710a18e1e71fb256e8451e87987510e
icefields <arctic.icefields@slmails.com>
2024-12-05 14:28:43 -0500

    added info for songs in stats script

--- stats.lua
+++ stats.lua
@@ -1,3 +1,17 @@
+-----------------------------------------------------
+-- ----------------------------------------------- --
+--   ▄        ▄     ▄  ▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄  ▄     ▄   --
+--  ▐░▌      ▐░▌   ▐░▌▐░█▀▀▀▀▀  ▀▀█░█▀▀ ▐░▌   ▐░▌  --
+--  ▐░▌      ▐░▌   ▐░▌▐░▌         ▐░▌   ▐░█   █░▌  --
+--  ▐░▌      ▐░▌   ▐░▌▐░▌         ▐░▌   ▐░░░░░░░▌  --
+--  ▐░▌      ▐░▌   ▐░▌▐░▌         ▐░▌    ▀▀▀▀▀█░▌  --
+--  ▐░█▄▄▄▄▄ ▐░█▄▄▄█░▌▐░█▄▄▄▄▄  ▄▄█░█▄▄       ▐░▌  --
+--   ▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀        ▀   --
+-- ----------------------------------------------- --
+-- ------- Luci4 util print Ampache stats -------- --
+-- -------- https://github.com/icefields --------- --
+-----------------------------------------------------
+
 local handshake = require("ampache-handshake")
 local http = require("socket.http")
 local ltn12 = require("ltn12")  -- For handling response body
@@ -7,6 +21,7 @@ local cjson = require("cjson")  -- For decoding JSON
 local limit = 10  -- Default limit
 local type_value = "album"  -- Default type
 local filter_value = "newest"  -- Default filter
+local is_json_output = false
 
 -- Function to check if a value is valid (not nil, empty, or 'null')
 local function isValid(value)
@@ -34,6 +49,7 @@ Optional arguments:
   -l <limit>     Limit the number of items to retrieve (default: 10)
   -t <type>      Specify the type of items to retrieve (valid values: album, song, artist, video, playlist, podcast, podcast_episode; default: album)
   -f <filter>    Specify the filter for the items (valid values: newest, highest, frequent, recent, forgotten, flagged, random; default: newest)
+  -j		 prints the original json from the network response, when this is passed, all other optional args are ignored
   -h             Show this help message
 ]])
 end
@@ -96,6 +112,8 @@ for i = 4, #arg do
             error("Invalid filter specified. Valid values are: newest, highest, frequent, recent, forgotten, flagged, random.")
         end
         i = i + 1  -- Skip the next argument
+    elseif arg_val == "-j" then
+	is_json_output = true
     end
 end
 
@@ -117,8 +135,15 @@ local res, code, response_headers, status = http.request{
 
 -- Check if the request was successful
 if code == 200 then
+    
     -- Join the response body into a string
     local json_response = table.concat(response_body)
+    
+    -- if the -j option is passed, just print the json file
+    if is_json_output == true then
+    	print(json_response)
+	return
+    end
 
     -- Decode the JSON response
     local data = cjson.decode(json_response)
@@ -133,8 +158,28 @@ if code == 200 then
             safePrint("artist", string.format("%s (id: %s)", item.artist.name, item.artist.id))
         end
 
+        -- Print album name if valid
+        if item.album then
+            safePrint("album", string.format("%s (id: %s)", item.album.name, item.album.id))
+        end
+
+        -- print song url if available
+        safePrint("url", item.url)
+        
         -- Print time if valid
         safePrint("time", item.time)
+
+        -- fields for song 
+        safePrint("playlisttrack", item.playlisttrack)
+        safePrint("format", item.format)
+        safePrint("stream_format", item.stream_format)
+        safePrint("stream_mime", item.stream_mime)
+        safePrint("bitrate", item.bitrate)
+        safePrint("stream_bitrate", item.stream_bitrate)
+        safePrint("rate", item.rate)
+        safePrint("mode", item.mode)
+        safePrint("mime", item.mime)
+        safePrint("stream_mime", item.stream_mime)
 
         -- Print year if valid
         safePrint("year", item.year)
