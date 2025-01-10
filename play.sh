f1c7df24d407fc0b9e645e27d9eb7d590afd3887
lucifer <arctic.icefields@slmails.com>
2025-01-10 09:59:36 -0500

    added script to play music with mpv

---
+++ play.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+selectsong() {
+    input_data=$(cat)
+    # oneline=$(awk 'BEGIN {RS=""; FS="\n"} {print $1 "\t" $2 "\n"}')
+    #awk 'BEGIN {RS=""; FS="\n"} {print $1 ": " $2 "\t" $3}' | \
+    printf "%s" "$input_data" | awk 'BEGIN {RS=""; FS="\n"} {print $1}' | \
+        showmenu | \
+        while read -r selection; do
+            # Extract the URL part from the selection (after the tab)
+            url=$(printf "%s" "$input_data" | grep -A 1 "$selection" | tail -n 1)
+
+            # Output only the URL
+            echo "$url"
+        done
+}
+
+showmenu() {
+    # Read input from standard input
+    input=$(cat)
+   
+    if command -v wofi >/dev/null 2>&1; then
+        echo "$input" | wofi --conf $HOME/.config/wofi/config-smenu --style $HOME/.config/wofi/styleS.css 
+    elif command -v dmenu >/dev/null 2>&1; then
+        echo "$input" | dmenu -i -l 25
+    elif command -v fzf >/dev/null 2>&1; then
+        echo "$input" | fzf
+    else
+        echo "Neither dmenu nor wofi is installed."
+        return 1
+    fi
+}
+
+
+playsong() {
+    songurl=$(cat)
+    mpv "$songurl"
+}
+
+# Check if exactly 3 parameters are provided
+if [ $# -eq 3 ]; then
+  serverurl=$1
+  username=$2
+  password=$3
+else
+  IFS=$'\n' read -d '' -r serverurl username password < $HOME/.config/ampache_credentials 
+fi
+
+song=$(echo "" | showmenu)
+lua songs.lua $serverurl $username $password -f "$song" | selectsong | playsong
