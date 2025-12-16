5ac16efcef11883435ecd01bbeb817843dc99e9d
lucifer <arctic.icefields@slmails.com>
2025-12-16 16:55:52 -0500

    removed useless files

--- download-songs-playlist.fish
+++ download-songs-playlist.fish
@@ -2,6 +2,6 @@
 
 lua playlist-songs.lua $argv[1] $argv[2] $argv[3] -j -f $argv[4] | jq -r '.song[] | .url' | while read url
     set filename (basename $url)
-    curl -o $HOME/Music/00_SOUL_DOWNLOADS/toconvert/$filename $url
+    curl -o $HOME/Music/ampachedownloads/$filename $url
 end
 
